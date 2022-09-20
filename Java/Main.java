664002f78a89b19dbcc72904b39353ea7b8a1a12
Sebastian Ricaurte <1jr@hotmail.com>
2022-09-19 21:33:14 -0500

    apuntes 10, POO

--- Java/Main.java
+++ Java/Main.java
@@ -6,14 +6,19 @@ class Main {
         //empecemos con atributos
         //car.license = "AMQ123"; se ahorro esto
         //car.driver = "Andres Herrera"; se ahorro esto
-        car.passegenger = 4;
+        //UberX.passegenger = 4;
+        UberX.setPassenger(4);
         //con esta ay tengo acceso a imprimir de car
-        car.printDataCAr();
+        UberX.printDataCar();
 
-        Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876"));
+        UberVan uberVan = new UberVan("FGH345", new Account("Andres Herrera", "AND123"));
+        uberVan.setPassenger(6);
+        uberVan.printDataCar();
+
+        /*Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876"));
         //car2.license = "qwe567";
         //car2.driver = "Andrea herrera";
         car2.passegenger = 3;
-        car2.printDataCAr();
+        car2.printDataCAr();*/
     }
 }
